24e2d298a830de1b8d00411c327d1e55f81bd436
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-17 11:13:22 +0100

    Do not convert compacted JSON-LD to Clojure data structure by default

--- src/rdf_path_examples/jsonld.cljs
+++ src/rdf_path_examples/jsonld.cljs
@@ -7,13 +7,14 @@
 
 (def path-context (js/JSON.parse (read-file "contexts/path.json")))
 
+(def example-context (js/JSON.parse (read-file "contexts/example.json")))
+
 ; ----- Public functions -----
 
 (defn compact-jsonld
   "Compact `json` serialized in JSON-LD using `context`."
   [json context]
-  (let [->clj (fn [json] (js->clj json :keywordize-keys true))
-        out (chan 1 (map (comp :graph ->clj)))]
+  (let [out (chan 1)]
     (js/jsonld.compact json
                        context
                        (fn [err compacted] (put! out compacted)))
