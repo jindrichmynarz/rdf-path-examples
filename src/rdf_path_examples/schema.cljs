31046895bb2fe8b598aa64b46566205a61b56f5b
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-20 23:08:24 +0100

    Crude Prismatic Schema for RDF paths

--- src/rdf_path_examples/schema.cljs
+++ src/rdf_path_examples/schema.cljs
@@ -1,5 +1,6 @@
 (ns rdf-path-examples.schema
-  (:require [schema.core :as s :include-macros true])
+  (:require [rdf-path-examples.prefixes :refer [rdfs]]
+            [schema.core :as s :include-macros true])
   (:import [goog Uri]))
 
 (defn- http-iri?
@@ -18,3 +19,27 @@
    :graph-iri HTTP-IRI
    :selection-method (s/enum "random")
    (s/optional-key :limit) (s/conditional positive? s/Int)})
+
+(def Node
+  "A node in path edge"
+  (s/conditional map? {:type s/Str ; A node must be explicitly typed.
+                       s/Keyword s/Any} ; Optional node predicates (include :type)
+                 string? s/Str))
+
+(def Edge
+  "An edge in RDF path"
+  {(s/optional-key :type) (s/pred (partial = "Edge"))
+   :start Node
+   :edgeProperty s/Str
+   :end Node})
+
+(def Path
+  "RDF path"
+  {:type (s/pred (partial = "Path")) ; Path must be explicitly typed.
+   :edges [Edge]
+   (s/optional-key :id) s/Str})
+
+(def PathGraph
+  "Graph of RDF paths"
+  (s/conditional (fn [graph] (some (comp nil? (partial s/check Path)) graph))
+                 [{s/Keyword s/Any}]))
