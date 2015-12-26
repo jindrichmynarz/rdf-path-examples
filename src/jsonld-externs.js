c70fee4736c675fc0e208841aac8437b483d434f
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-26 16:45:17 +0100

    More externs for jsonld.js

--- src/jsonld-externs.js
+++ src/jsonld-externs.js
@@ -1,2 +1,4 @@
 jsonld = function() {};
 jsonld.compact = function(input, ctx, options, callback) {};
+jsonld.toRDF = function(input, options, callback) {};
+jsonld.fromRDF = function(input, options, callback) {};
