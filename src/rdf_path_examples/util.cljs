f9ff1e01ef72a1cac3e9aa1aa12cc2ec356416de
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-26 19:00:26 +0100

    Add logging helper function

--- src/rdf_path_examples/util.cljs
+++ src/rdf_path_examples/util.cljs
@@ -1,6 +1,13 @@
 (ns rdf-path-examples.util
   (:require [cljsjs.mustache :as mustache]))
 
+(enable-console-print!)
+
+(defn log
+  "Log Clojure `data` as a stringified and pretty-printed JSON."
+  [data]
+  (println (js/JSON.stringify (clj->js data) nil "  ")))
+
 (defn render-template
   "Render Mustache template with data."
   [template & {:keys [data]}]
