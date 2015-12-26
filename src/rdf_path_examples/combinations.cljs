2830c079efad04890d130ac5d865c62b4742ca5c
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-26 16:45:32 +0100

    Commas are whitespace

--- src/rdf_path_examples/combinations.cljs
+++ src/rdf_path_examples/combinations.cljs
@@ -4,7 +4,7 @@
 (defn- index-combinations
   [n cnt]
   (lazy-seq
-    (let [c (vec (cons nil (for [j (range 1 (inc n))] (+ j cnt (- (inc n)))))),
+    (let [c (vec (cons nil (for [j (range 1 (inc n))] (+ j cnt (- (inc n))))))
           iter-comb
           (fn iter-comb [c j]
             (if (> j n) nil
@@ -12,7 +12,7 @@
                 (if (< (c j) j) [c (inc j)]
                   (loop [c c, j j]
                     (if (= j 1) [c j]
-                      (recur (assoc c (dec j) (dec (c j))) (dec j)))))))),
+                      (recur (assoc c (dec j) (dec (c j))) (dec j))))))))
           step
           (fn step [c j]
             (cons (rseq (subvec c 1 (inc n)))
@@ -43,20 +43,20 @@
         (conj (pop distribution) [(inc index) 1 this-and-to-the-left])
         (conj (pop distribution) 
               [index (dec this-bucket) (dec this-and-to-the-left)]
-              [(inc index) 1 this-and-to-the-left])),
+              [(inc index) 1 this-and-to-the-left]))
       ; so we have stuff in the last bucket
       (= this-bucket total) nil
       :else
-      (loop [distribution (pop distribution)],
+      (loop [distribution (pop distribution)]
         (let
-          [[index this-bucket this-and-to-the-left] (peek distribution),
+          [[index this-bucket this-and-to-the-left] (peek distribution)
            distribution (if (= this-bucket 1) 
                           (pop distribution)
                           (conj (pop distribution)
-                                [index (dec this-bucket) (dec this-and-to-the-left)]))],
+                                [index (dec this-bucket) (dec this-and-to-the-left)]))]
           (cond
             (<= (- total (dec this-and-to-the-left)) (apply + (subvec m (inc index))))       
-            (distribute m (inc index) total distribution (dec this-and-to-the-left)),
+            (distribute m (inc index) total distribution (dec this-and-to-the-left))
             
             (seq distribution) (recur distribution)
             :else nil))))))
@@ -76,8 +76,8 @@
 (defn- multi-comb
   "Handles the case when you want the combinations of a list with duplicate items."
   [l t]
-  (let [f (frequencies l),
-        v (vec (distinct l)),
+  (let [f (frequencies l)
+        v (vec (distinct l))
         domain (range (count v))
         m (vec (for [i domain] (f (v i))))
         qs (bounded-distributions m t)]
@@ -96,5 +96,5 @@
               (= t 1) (for [item (distinct items)] (list item))
               (distinct? items) (if (= t cnt) 
                                   (list (seq items))
-                                  (map #(map v-items %) (index-combinations t cnt))),
+                                  (map #(map v-items %) (index-combinations t cnt)))
               :else (multi-comb items t))))))
