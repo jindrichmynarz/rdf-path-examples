04defa7939d2cb16cd5066715a52498ae7aec317
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-28 12:12:48 +0100

    Update test runner

--- test/rdf_path_examples/runner.cljs
+++ test/rdf_path_examples/runner.cljs
@@ -2,7 +2,9 @@
   (:require [cljs.test :refer-macros [run-all-tests]]
             [doo.runner :refer-macros [doo-tests]]
             [rdf-path-examples.path-test]
-            [rdf-path-examples.sparql-test]))
+            [rdf-path-examples.sparql-test]
+            [rdf-path-examples.type-inference-test]))
 
 (doo-tests 'rdf-path-examples.path-test
-           'rdf-path-examples.sparql-test)
+           'rdf-path-examples.sparql-test
+           'rdf-path-examples.type-inference-test)
