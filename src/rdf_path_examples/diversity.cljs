5f9cd7170a75f707749dc0ec5ec12ad18e9f08e1
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-30 16:21:14 +0100

    Roll back from transducers in greedy construction

--- src/rdf_path_examples/diversity.cljs
+++ src/rdf_path_examples/diversity.cljs
@@ -1,25 +1,29 @@
 (ns rdf-path-examples.diversity)
 
+; ----- Private functions -----
+
 (defn- min-similarity
   [[p1 s1] [p2 s2]]
   (if (< s1 s2) [p1 s1] [p2 s2]))
 
+; ----- Public functions -----
+
 (defn greedy-construction
   "Select `n` most diverse `paths` using `path-similarities` based on the greedy construction heuristic."
-  [paths path-similarities n]
-  (let [start (rand-nth (vec paths))
-        [solutions] (transduce (filter (comp (partial contains? start) first))
-                               min-similarity
-                               path-similarities)
+  [^ISet paths
+   ^IMap path-similarities
+   ^Number n]
+  (let [start (rand-nth (vec paths)) ; Randomly select 1 path.
+        [solutions] (reduce min-similarity
+                            (filter (comp #(contains? % start) first) path-similarities))
         aggregated-similarity (fn [solutions candidate]
                                    [candidate
                                     (transduce (map (comp path-similarities (partial set candidate)))
                                                +
                                                solutions)])
         generate-solution (fn [candidates solutions]
-                            (ffirst (transduce (map (partial aggregated-similarity solutions)) 
-                                               min-similarity
-                                               candidates)))]
+                            (first (reduce min-similarity
+                                            (map (partial aggregated-similarity solutions) candidates))))]
     (loop [solutions solutions
            candidates (apply disj paths solutions)
            solutions-count 2]
