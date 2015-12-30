296de3672459762e7e33a35863fb9b68f95af703
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-12-30 16:23:46 +0100

    Throw out SPARQL tests

--- test/rdf_path_examples/runner.cljs
+++ test/rdf_path_examples/runner.cljs
@@ -2,9 +2,7 @@
   (:require [cljs.test :refer-macros [run-all-tests]]
             [doo.runner :refer-macros [doo-tests]]
             [rdf-path-examples.path-test]
-            [rdf-path-examples.sparql-test]
             [rdf-path-examples.type-inference-test]))
 
 (doo-tests 'rdf-path-examples.path-test
-           'rdf-path-examples.sparql-test
            'rdf-path-examples.type-inference-test)
