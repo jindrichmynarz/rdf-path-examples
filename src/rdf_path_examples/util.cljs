8a5f41c568a10f8c5515ff38d39ced742a1b3acf
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-01-01 19:42:58 +0100

    JSON-LD navigation helper functions

--- src/rdf_path_examples/util.cljs
+++ src/rdf_path_examples/util.cljs
@@ -7,6 +7,26 @@
   "Regular expression matching xsd:duration."
   #"^-?P(\d+Y)?(\d+M)?(\d+D)?T?(\d+H)?(\d+M)?(\d+(\.\d+)?S)?$")
 
+(defn find-by-id
+  "Helper function to find a resource identified with `id` in `resources`."
+  [resources id & {:keys [keywordized?]
+                   :or {keywordized? true}}]
+  (let [get-id (if keywordized? :id #(get % "@id"))
+        matches (filter (comp (partial = id) get-id) resources)]
+    (if (seq matches)
+      (first matches)
+      {"@id" id})))
+
+(defn resolve-resource
+  "Resolve a `resource` by using `data`.
+  Returns map or vector of maps."
+  [data
+   {id "@id"
+    :as resource}]
+  (cond (string? resource) {"@value" resource}
+        (map? resource) (if id (find-by-id data id :keywordized? false) resource)
+        (coll? resource) (mapv (partial resolve-resource data) resource)))
+
 (defn log
   "Log Clojure `data` as a stringified and pretty-printed JSON."
   [data]
