774ae551b51e00625783da1fbd39c2a2d5aa4b78
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-01-01 19:43:58 +0100

    DRY test

--- test/rdf_path_examples/similarity_test.cljs
+++ test/rdf_path_examples/similarity_test.cljs
@@ -3,14 +3,14 @@
             [cljs.test :refer-macros [are deftest is testing]]))
 
 (deftest compute-similarity-test
-  (testing "Equivalent resources have similarity of 1"
-    (are [resource] (= (compute-similarity (fn [_]) resource resource) 1)
-         {"@value" 1}
-         {"@value" "2015-12-30"}
-         {"@value" "https://example.com:3030/path/to/resource"}))
-  (testing "Similarity is symmetric"
-    (let [similarity-fn (partial compute-similarity (fn [_]))]
-    (are [a b] (= (similarity-fn a b) (similarity-fn b a))
-         {"@value" 1} {"@value" 3}
-         {"@value" "http://example.com/path/to/a/file"} {"@value" "https://example.com/path/to/another/file"}
-         {"@id" "_:b1"} {"@id" "_:b2"}))))
+  (let [similarity-fn (partial compute-similarity (fn [_]))] ; Mocked similarity function
+    (testing "Equivalent resources have similarity of 1"
+      (are [resource] (= (similarity-fn resource resource) 1)
+           {"@value" 1}
+           {"@value" "2015-12-30"}
+           {"@value" "https://example.com:3030/path/to/resource"}))
+    (testing "Similarity is symmetric"
+      (are [a b] (= (similarity-fn a b) (similarity-fn b a))
+           {"@value" 1} {"@value" 3}
+           {"@value" "http://example.com/path/to/a/file"} {"@value" "https://example.com/path/to/another/file"}
+           {"@id" "_:b1"} {"@id" "_:b2"}))))
