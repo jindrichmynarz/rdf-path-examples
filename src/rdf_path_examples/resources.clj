74085d269b1b9618f4f4f39ed76aa99a64402b43
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-03-23 13:34:25 +0100

    Amended the flow of POST request

--- src/rdf_path_examples/resources.clj
+++ src/rdf_path_examples/resources.clj
@@ -3,29 +3,39 @@
             [rdf-path-examples.jsonld :as jsonld]
             [rdf-path-examples.integrity-constraints :refer [validate-path]]
             [rdf-path-examples.examples :refer [generate-examples]]
+            [rdf-path-examples.views :as views]
             [schema.coerce :as coerce]
             [clojure.tools.logging :as log]
-            [liberator.core :refer [defresource]]))
+            [clojure.walk :refer [keywordize-keys]]
+            [liberator.core :refer [defresource]]
+            [liberator.representation :refer [render-map-generic]]))
+
+(def ^:private default-response
+  {:representation {:media-type "application/ld+json"}})
 
 (def parse-query-params
   (coerce/coercer Config coerce/string-coercion-matcher))
 
+(defmethod render-map-generic "application/ld+json"
+  ; Alias the JSON rendering function for JSON-LD
+  [data context]
+  ((get-method render-map-generic "application/json") data context))
+
 (defresource rdf-path-examples
   :allowed-methods [:post]
   :available-media-types ["application/ld+json"]
+  :handle-malformed views/error
   :handle-ok (fn [{{configuration :params} :request
                    :keys [rdf-path]}]
-               {:message (generate-examples configuration rdf-path)
-                :representation {:media-type "application/ld+json"}})
+               (generate-examples configuration rdf-path))
   :known-content-type? (fn [{{{content-type "content-type"} :headers} :request}]
                          (= content-type "application/ld+json"))
   :malformed? (fn [{{:keys [body query-params]} :request}]
                 (let [configuration (parse-query-params query-params)
                       path (jsonld/json-ld->rdf-model body)]
                   (if-let [error (or (:error configuration) (validate-path path))]
-                    [true {:message (str error)
-                           :representation {:media-type (if (string? error)
-                                                          "application/ld+json"
-                                                          "text/plain")}}]
-                    [false {:request {:params (merge {:limit 5} configuration)}
-                            :rdf-path path}]))))
+                    [true (assoc default-response :error-msg error)]
+                    [false {:request {:params (merge {:limit 5} (keywordize-keys configuration))}
+                            :rdf-path path}])))
+  :new? (constantly false)
+  :respond-with-entity? (constantly true))
