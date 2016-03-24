a4054a44990c2b7f98714a3a0af1dcf8cb5ee16d
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-03-24 11:00:11 +0100

    JSON-LD convenience loaders

--- src/rdf_path_examples/json_ld.clj
+++ src/rdf_path_examples/json_ld.clj
@@ -1,7 +1,9 @@
 (ns rdf-path-examples.json-ld
+  (:require [clojure.java.io :as io])
   (:import [java.io InputStream]
            [java.util LinkedHashMap]
            [com.github.jsonldjava.core JsonLdOptions JsonLdProcessor]
+           [com.github.jsonldjava.utils JsonUtils]
            [org.apache.jena.rdf.model Model ModelFactory]))
 
 (defonce ^:private
@@ -13,10 +15,18 @@
   [^InputStream json-ld]
   (.read (ModelFactory/createDefaultModel) json-ld nil "JSON-LD"))
 
-(defn compact-json-ld
+(defn compact
   "Compact `json-ld` using `context` with optional `options`"
   [^LinkedHashMap json-ld
    ^LinkedHashMap context
    & {:keys [options]
       :or {options json-ld-options}}]
   (JsonLdProcessor/compact json-ld context options))
+
+(def load-resource
+  "Load JSON resource from `resource-path`."
+  (comp #(JsonUtils/fromInputStream %) io/input-stream io/resource))
+
+(def load-context
+  "Load JSON-LD context with `file-name`."
+  (comp load-resource (partial str "contexts/")))
