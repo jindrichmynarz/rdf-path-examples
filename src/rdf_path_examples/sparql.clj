499e6cd3d90ae991412ed9aac98a91fdca30c8e0
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-03-27 16:14:29 +0200

    Enable SPARQL Update

--- src/rdf_path_examples/sparql.clj
+++ src/rdf_path_examples/sparql.clj
@@ -1,8 +1,9 @@
 (ns rdf-path-examples.sparql
   (:require [clojure.tools.logging :as log])
-  (:import [org.apache.jena.rdf.model Literal Resource]
+  (:import [org.apache.jena.rdf.model Literal Model Resource]
            [org.apache.jena.query QueryExecutionFactory QueryFactory]
-           [org.apache.jena.query Dataset]))
+           [org.apache.jena.query Dataset]
+           [org.apache.jena.update UpdateAction UpdateFactory]))
 
 ; ----- Protocols -----
 
@@ -32,21 +33,21 @@
 ; ----- Public functions -----
 
 (defn ^Boolean ask-query
-  "Execute SPARQL ASK `query` on RDF `dataset`."
-  [^Dataset dataset
+  "Execute SPARQL ASK `query` on RDF `model`."
+  [^Model model
    ^String query]
-  (with-open [qexec (QueryExecutionFactory/create query dataset)]
+  (with-open [qexec (QueryExecutionFactory/create query model)]
     (.execAsk qexec)))
 
 (defmulti construct-query
   "Execute SPARQL CONSTRUCT `query` on `datasource`."
   (fn [datasource query] (type datasource)))
 
-(defmethod ^Model construct-query Dataset
-  ; Execute SPARQL CONSTRUCT `query` on local RDF `dataset`.
-  [^Dataset dataset
+(defmethod ^Model construct-query Model
+  ; Execute SPARQL CONSTRUCT `query` on local RDF `model`.
+  [^Model model
    ^String query]
-  (with-open [qexec (QueryExecutionFactory/create query dataset)]
+  (with-open [qexec (QueryExecutionFactory/create query model)]
     (.execConstruct qexec)))
 
 (defmethod ^Model construct-query String
@@ -60,12 +61,19 @@
   "Execute SPARQL SELECT `query` on `datasource`." 
   (fn [datasource query] (type datasource)))
 
-(defmethod select-query Dataset
-  ; Execute SPARQL SELECT `query` on local RDF `dataset`.
-  [^Dataset dataset
+(defmethod select-query Model
+  ; Execute SPARQL SELECT `query` on local RDF `model`.
+  [^Model model
    ^String query]
-  (with-open [qexec (QueryExecutionFactory/create query dataset)]
+  (with-open [qexec (QueryExecutionFactory/create query model)]
     (let [results (.execSelect qexec)
           result-vars (.getResultVars results)]
       (mapv (partial process-select-solution result-vars)
             (iterator-seq results)))))
+
+(defn ^Model update-operation
+  "Execute SPARQL Update `operation` on `model`."
+  [^Model model
+   ^String operation]
+  (UpdateAction/execute (UpdateFactory/create operation) model)
+  model)
