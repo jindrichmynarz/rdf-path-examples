4792ef5eb956ba07fb310ab1aa23de153102b6bf
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-03-31 11:02:49 +0200

    Expose sampling factor as a parameter

--- src/rdf_path_examples/schema.clj
+++ src/rdf_path_examples/schema.clj
@@ -4,9 +4,13 @@
 
 (def http? (partial re-matches #"^https?:\/\/.*$"))
 
+(def positive-integer
+  (s/constrained s/Int pos? 'pos?))
+
 (def Config
   "Schema for configuration parameters"
   {(s/required-key "sparql-endpoint") (s/constrained sc/URI http? 'http?) 
    (s/required-key "selection-method") (s/enum "random" "distinct" "representative")
    (s/optional-key "graph-iri") sc/URI
-   (s/optional-key "limit") (s/constrained s/Int pos? 'pos?)})
+   (s/optional-key "limit") positive-integer
+   (s/optional-key "sampling-factor") positive-integer})
