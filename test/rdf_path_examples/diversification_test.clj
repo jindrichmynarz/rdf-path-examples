e785868b5053e2deaaaf13117c8460ec6991762e
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-04-12 22:34:55 +0200

    Move redefs

--- test/rdf_path_examples/diversification_test.clj
+++ test/rdf_path_examples/diversification_test.clj
@@ -4,9 +4,9 @@
             [clojure.test :refer :all]))
 
 (deftest greedy-construction
-  (with-redefs [rand-nth (constantly 1)] ; Always start with path 1
-    (let [distances {#{1 2} 0.5
-                     #{1 3} 1
-                     #{2 3} 0.5}
-          paths (apply union (keys distances))]
+  (let [distances {#{1 2} 0.5
+                   #{1 3} 1
+                   #{2 3} 0.5}
+        paths (apply union (keys distances))]
+    (with-redefs [rand-nth (constantly 1)] ; Start with path 1
       (is (= (divers/greedy-construction paths distances 2) #{1 3})))))
