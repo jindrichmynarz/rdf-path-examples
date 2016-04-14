adb415aaaa7721a16069f00f69d07001512896ba
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-04-14 16:46:11 +0200

    Test greedy construction for 3 selected items

--- test/rdf_path_examples/diversification_test.clj
+++ test/rdf_path_examples/diversification_test.clj
@@ -6,8 +6,11 @@
 (deftest greedy-construction
   (let [distances {#{1 2} 0.5
                    #{1 3} 1
-                   #{2 3} 0.5}
+                   #{1 4} 1
+                   #{2 3} 0.5
+                   #{2 4} 0.5
+                   #{3 4} 0}
         distance-fn (fn [a b] (get distances (hash-set a b)))
         paths (apply union (keys distances))]
     (with-redefs [rand-nth (constantly 1)] ; Start with path 1
-      (is (= (divers/greedy-construction paths distance-fn 2) #{1 3})))))
+      (is (= (divers/greedy-construction paths distance-fn 3) #{1 2 3})))))
