8ec7e9660eae9828e3e5c2edbb51cf8077a4f13f
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-04-26 14:41:32 +0200

    Print progress indicator during evaluation

--- src/rdf_path_examples/evaluation.clj
+++ src/rdf_path_examples/evaluation.clj
@@ -47,14 +47,18 @@
            number-of-runs 10}}]
   (let [paths (map (comp json-ld->rdf-model io/input-stream)
                    (filter #(.isFile %) (file-seq (io/as-file path-dir))))
+        path-count (count paths)
         generate-fn (fn [path] (try-times number-of-retries (examples/generate-examples params path)))
         clj->rdf-model (comp json-ld->rdf-model string->input-stream json/generate-string)]
     (doall (mapv (comp average
-                       (fn [path] (repeatedly number-of-runs
-                                              (comp (partial intra-list-diversity params)
-                                                    clj->rdf-model
-                                                    (partial generate-fn path)))))
-                 paths))))
+                       (fn [[path index]] 
+                         (println (str "Processing path " index "/" path-count))
+                         (repeatedly number-of-runs
+                                     (comp (partial intra-list-diversity params)
+                                           clj->rdf-model
+                                           (partial generate-fn path)))))
+                 paths
+                 (range 1 (inc path-count))))))
 
 (defn mann-whitney-u-test
   "Compute Mann-Whitney U test between collections of doubles `a` and `b`."
